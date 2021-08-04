84ce0a3651d93f669b265addf47b2e3428070ccb
onguc <ongucirfan@gmail.com>
2021-08-04 12:41:10 +0300

    #0.0.7 sürüm alırken web'te çıkan hata için try catch eklendi

--- lib/src/utils/util.dart
+++ lib/src/utils/util.dart
@@ -8,12 +8,21 @@ import 'package:package_info/package_info.dart';
 class Util {
   static String? _version = "";
   static String deviceId = "";
-  static final bool isNotWeb = (Platform.isAndroid ||
-      Platform.isIOS ||
-      Platform.isWindows ||
-      Platform.isMacOS ||
-      Platform.isLinux ||
-      Platform.isFuchsia);
+  static final bool isNotWeb = _isPlatformWeb;
+
+  static bool get _isPlatformWeb {
+    try {
+      bool isNotWeb = (Platform.isAndroid ||
+          Platform.isIOS ||
+          Platform.isWindows ||
+          Platform.isMacOS ||
+          Platform.isLinux ||
+          Platform.isFuchsia);
+      return isNotWeb;
+    } catch (e) {
+      return false;
+    }
+  }
 
   static Future<String?>? get version async {
     if (_version == null && isNotWeb) {
