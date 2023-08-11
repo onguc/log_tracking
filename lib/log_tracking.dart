7de71898bb7784fa829a01d68782eb538b8805ec
onguc <ongucirfan@gmail.com>
2023-08-11 09:04:49 +0300

    #0.1.21 web için crashlytic kullanımı engellendi

--- lib/log_tracking.dart
+++ lib/log_tracking.dart
@@ -1,6 +1,4 @@
 library log_tracking;
-import 'dart:html';
-
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:hive_flutter/adapters.dart';
